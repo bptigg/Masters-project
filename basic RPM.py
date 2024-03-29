e83019f8d46e998d705f1a2f8be319b19b80f973
Benji Tigg <bptigg@gmail.com>
2024-03-29 17:11:22 +0000

    fixing multiprocessing
    fixed the issues with pipes and the main thread trying to use pipes that where closed by putting a duplex pipe in, so both sockets have read and write permission to the pipe

--- basic RPM.py
+++ basic RPM.py
@@ -5,8 +5,9 @@ from scipy import sparse
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import qutip as qt
-from multiprocessing import Process
 import multiprocessing
+import datetime
+import select
 
 
 operator_function_mapping = {
@@ -29,7 +30,7 @@ model_parameters = {
     'B_0' : 1.4 * 2 * np.pi,
     'k_b' : 2., #1/us
     'k_f' : 1.,
-    'angle_step' : 2*np.pi / 3,
+    'angle_step' : 2*np.pi / 72,
     'run_time' : 20,  
     'absolute tolerance' : 1e-10,
     'relative tolerance' : 1e-8
@@ -46,6 +47,9 @@ Nuclei = {
                                            [0.0460172, -0.564764,  0.453074]])
     
 }
+
+
+
 
 class structure:
     nuclear_spins = []
@@ -109,7 +113,7 @@ def make_Hamiltonian_2(dims, ind_1, ind_2, parmat):
         return zero_operator(dims)
 
 def point_dipole_dipole_coupling(r):
-    C = -1* (constants['mu_naught'] / (4*np.pi*1e-30)) * (constants['g_electron'] * constants['mu_bohr'])**2 * 1/(1e6*constants['h_bar'])
+    C = -1* (constants['mu_naught'] / (4*np.pi*1e-30)) * (constants['g_electron'] * constants['mu_bohr'])**2 * 1/(1e6*constants['h_bar']*2*np.pi)
     
     if np.isscalar(r):
         d = C / r**3
@@ -138,7 +142,7 @@ def radical_pair_model(model : structure, processes : int):
 
     initial_projection_operator = 1/4 * make_spin_operator(dims, []) - make_Hamiltonian_2(dims, 0, 1, np.identity(3)) 
     identity = make_spin_operator(dims, [])
-    recombination_projection_operator = identity - initial_projection_operator
+    triplet_projection_operator = identity - initial_projection_operator
 
     Hyperfine_hamiltonian = sum(make_Hamiltonian_2(dims, i, i+2, model.get_hyperfine_matracies(i)) for i in range(model.get_hyperfine_matracies(length=True)))
     dipole_dipole_electron_hamiltonian = make_Hamiltonian_2(dims, 0, 1, DDSE)
@@ -153,14 +157,14 @@ def radical_pair_model(model : structure, processes : int):
     new = False
 
     if(steps_2 == steps):
-        orientation = [*[np.arange(0 , 2* np.pi , model_parameters['angle_step'],) for i in range(0,steps_2)]]
+        orientation = [*[np.arange(0 , 2* np.pi + model_parameters['angle_step']  , model_parameters['angle_step'],) for i in range(0,steps_2+1)]]
     else:
         new_step = (np.pi / steps_2)
         new = True
-        orientation = [*[np.arange(0, 2* np.pi, new_step) for i in range(0,steps_2)]]
+        orientation = [*[np.arange(0, 2* np.pi + new_step, new_step) for i in range(0,steps_2+1)]]
 
     t_max = model_parameters['run_time'] / model_parameters['k_f']
-    t_list = np.linspace(0,t_max, int(np.ceil(1000*t_max)))
+    #t_list = np.linspace(0,t_max, int(np.ceil(1000*t_max)))
 
     #tolerance options
     global opt
@@ -168,28 +172,28 @@ def radical_pair_model(model : structure, processes : int):
     opt.atol = model_parameters['absolute tolerance']
     opt.rtol = model_parameters['relative tolerance']
 
-    
-
-    yields = []
-    index = 0
     step = 0
+    global operation_number
+    operation_number = 0
+    operation_lock = multiprocessing.Lock()
 
     global len_theta
     global operations
-
-    len_theta  = len(orientation[0])
-    operations = len(orientation) * len_theta
-
-
-    #fig = plt.figure()
-    #ax = plt.axes(projection='3d')
-
-    first = True
 
     if new:
         step = new_step
     else:
         step = model_parameters['angle_step']
+
+    if(processes > len(orientation)):
+        processes = len(orientation)
+
+    orientations = []
+    size = len(orientation) / processes
+    size = int(np.ceil(size))
+    processes = int(len(orientation) / size)
+    for i in range(0,processes):
+        orientations.append(orientation[i*size : (i+1)*size])
 
     number_of_cores = multiprocessing.cpu_count()
     cores_per_process = float(number_of_cores) / float(processes)
@@ -197,83 +201,115 @@ def radical_pair_model(model : structure, processes : int):
     opt.num_cpus = cores_per_process
     opt.openmp_threads = cores_per_process
 
-    return_data = []
+    len_theta  = len(orientation[0])
+    operations = (len(orientation) * len_theta) / processes
+
 
     process_id = []
+    pipe = []
     for i in range(0,processes):
-        process_id.append()
+        if __name__ == '__main__':
+            reciever, sender = multiprocessing.Pipe(True)
+            reciever_2, sender_2 = multiprocessing.Pipe(True)
+            p = multiprocessing.Process(target = simulation, args = (dims, H_0, orientations[i], step, rho_0, initial_projection_operator, t_max, K, operation_lock, i * size, [sender, reciever_2]))
+            process_id.append(p)
+            pipe.append([reciever, sender_2])
 
-    
+    none_list = []
+    for p in process_id:
+        p.start()
+        none_list.append(False)
 
-    simulation(dims, H_0, orientation, step, rho_0, initial_projection_operator, t_max, K, return_data)
+    if __name__ == '__main__':
 
+        results = []
 
-    
-    ##for phi in orientation:
-    ##    yeild_phi = []
+        while(len(process_id) != 0):
+            remove = []
+            for i in range(0,len(process_id)):
+                joined = False
 
-    ##    x = []
-    ##    y = []
-    ##    z = []
-    ##    index_2 = 0
-    ##    for theta in phi:
-    ##        ori = polar_to_cart(theta, index * step)
+                #if(pipe[i][1].poll(0.01)):
+                #    if(pipe[i][1].recv() == None):
+                #        print("Process {} ended".format(process_id[i]._identity))
+                #
+                #elif(pipe[i][0].poll(0.01) == True):
+                #    results.append(pipe[i][0].recv())
+                #    if(pipe[i][1].closed == False):
+                #        if(process_id[i].is_alive() == True):
+                #            pipe[i][1].send(True)
 
-    ##        if first == False and (theta == np.pi or theta == 0):
+                #process_id[i].join(0.01)
+                #if(process_id[i].is_alive() == False):
+                #    remove.append(True)
+                #    joined = True
+                #else:
+                #    remove.append(False)
 
-    ##            print("Operation: {}/{}".format(index*len_theta + index_2 + 1, operations))
-    ##            index_2 = index_2 + 1
+                process_id[i].join(0.01)
+                if(process_id[i].is_alive() == False):
+                    remove.append(True)
+                else:
+                    remove.append(False)
+                
+                if(none_list[i] != True):
+                    if(pipe[i][0].poll(0.01) == True):
+                        result = pipe[i][0].recv()
+                        if(result == None):
+                            none_list[i] = True
+                        else:
+                            results.append(result)
 
-    ##            continue
-    ##        
-    ##        x.append(ori[0])
-    ##        y.append(ori[1])
-    ##        z.append(ori[2])
+                
+                
+                #if(joined != True and pipe[i][0].closed == False):
+                #    if(pipe[i][0].poll(0.1) == True):
+                #        results.append(pipe[i][0].recv())
+                #        if(pipe[i][1].closed == False):
+                #            pipe[i][1].send(True)
+                
 
-    ##        B_field = model_parameters['B_0'] * ori
+            removed = 0
+            for i in range(0, len(remove)):
+                if remove[i]:
+                    del(process_id[i-removed])
+                    del(pipe[i-removed])
+                    removed = removed+1
 
-    ##        H_zee = make_Hamiltionian(dims, 0, B_field) + make_Hamiltionian(dims, 1, B_field)
-    ##        H_eff = H_0 + H_zee - 1j*K
+        #results = [x.recv() for x in pipe]
 
-    ##        L_eff = -1j*qt.spre(H_eff) + 1j*qt.spost(H_eff.conj().trans())
-    ##        sol = qt.mesolve(L_eff, rho_0, t_list, e_ops=[initial_projection_operator], options=opt, progress_bar=True)
-    ##        ps = sol.expect[0]
-    ##        yr = model_parameters['k_b'] * integrate.simps(ps, t_list)
-    ##        yeild_phi.append(yr)
-    ##        print("Operation: {}/{}".format(index*len_theta + index_2 + 1, operations))
-    ##        index_2 = index_2 + 1
-    ##        #yeild_phi.append(0)
+        data = [[],[],[],[]]
+        for i in results:
+            list = 0
+            for e in i:
+                for f in range(0,len(e)):
+                    data[list].append(e[f])
+                list = list + 1
 
-    ##    yields.append(yeild_phi)
-    ##    first = False
-    ##    #p = ax.scatter3D(x,y,z, c = yeild_phi, cmap = 'viridis')
-    ##    index = index + 1
-    
-    print(yields)
-    #plt.show()
+        now = datetime.datetime.now()
+        dt_string = now.strftime("%d_%m_%Y %H_%M_%S")
+        f = open(dt_string + ".txt", "w")
 
+        for i in range(0,len(data[0])):
+            line = str(data[0][i]) + " " + str(data[1][i]) + " " + str(data[2][i]) + " " + str(data[3][i]) + "\n"
+            f.write(line)
+        f.close()
 
-def simulation(dims, H_0, orientation, step, rho_0, ps, t_max, K, return_data):
-
+def simulation(dims, H_0, orientation, step, rho_0, Ps, t_max, K, operation_lock : multiprocessing.Lock , start_index, return_data):
+    global operation_number
     t_list = np.linspace(0,t_max, int(np.ceil(1000*t_max)))
     
     x = []
     y = []
     z = []
     yields = []
+    index = start_index
+    send_data_permission = True
 
     for phi in orientation:
-        index_2 = 0
         for theta in phi:
             ori = polar_to_cart(theta, index * step)
 
-            if first == False and (theta == np.pi or theta == 0):
-
-                print("Operation: {}/{}".format(index*len_theta + index_2 + 1, operations))
-                index_2 = index_2 + 1
-
-                continue
-            
             x.append(ori[0])
             y.append(ori[1])
             z.append(ori[2])
@@ -283,23 +319,54 @@ def simulation(dims, H_0, orientation, step, rho_0, ps, t_max, K, return_data):
             H_zee = make_Hamiltionian(dims, 0, B_field) + make_Hamiltionian(dims, 1, B_field)
             H_eff = H_0 + H_zee - 1j*K
 
-            L_eff = -1j*qt.spre(H_eff) + 1j*qt.spost(H_eff.conj().trans())
-            sol = qt.mesolve(L_eff, rho_0, t_list, e_ops=[ps], options=opt, progress_bar=True)
-            ps = sol.expect[0]
-            yr = model_parameters['k_b'] * integrate.simps(ps, t_list)
-            yields.append(yr)
-            print("Operation: {}/{}".format(index*len_theta + index_2 + 1, operations))
-            index_2 = index_2 + 1
+            #L_eff = -1j*qt.spre(H_eff) + 1j*qt.spost(H_eff.conj().trans())
+            #sol = qt.mesolve(L_eff, rho_0, t_list, e_ops=[Ps], options=opt)
+            #ps = sol.expect[0]
+            #yr = model_parameters['k_b'] * integrate.simps(ps, t_list)
+            #yields.append(yr)
+            yields.append(ori[2]+ori[1]+ori[0])
+            operation_lock.acquire()
+            operation_number = operation_number + 1
+            
+            while(send_data_permission != True):
+                if(return_data[0].poll(0.1) == True):
+                    send_data_permission = return_data[0].recv()
+                
+            return_data[0].send([x,y,z,yields])
+            x.clear()
+            y.clear()
+            z.clear()
+            yields.clear()
 
-        first = False
-        index = index + 1
+            #if(operation_number % 10 == 0):
+            #    #have a pipe that sends a boolean back from the main thread to tell the sub threads that it's okay to send data back up the pipe    
+            #    if(send_data_permission == False):
+            #        if(return_data[1].poll(0.1) == True):
+            #            permission = [return_data[1].recv()]      
+            #            send_data_permission = permission[0]
+            #    return_data[0].send([x,y,z,yields])
+            #    send_data_permission = False
+            #    x.clear()
+            #    y.clear()
+            #    z.clear()
+            #    yields.clear()
 
-    return_data.append[[x],[y],[z],[yields]]
-    return return_data
+            print("Operation: {}/{}".format(operation_number, operations))
+            operation_lock.release()
+        index = index + 1  
+    
+    #send_data_permission = False
+    #while(send_data_permission == False):
+    #    if(return_data[1].poll() == True):
+    #        permission = [return_data[1].recv()]      
+    #        send_data_permission = permission[0]
+
+    return_data[0].send(None)
 
 
 def main():
-    radical_pair_model(structure([1,1], [Nuclei['N5'], Nuclei['N1']], model_parameters['rfW']), 4)
+    radical_pair_model(structure([1,1], [Nuclei['N5'], Nuclei['N1']], model_parameters['rfW']), 6)
+
 
 
 main()
